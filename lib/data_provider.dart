012466bb7dfc662f1e37f2e45dc15ed6b5d535d2
Jacky Tjoa <jacky.coolheart@gmail.com>
2021-04-15 22:21:49 +0800

    add data provider

---
+++ lib/data_provider.dart
@@ -0,0 +1,14 @@
+class DataProvider {
+  final _todoList = <String>[];
+
+  List<String> getTodoList() {
+    _todoList.add("A");
+    _todoList.add("B");
+    _todoList.add("C");
+    return _todoList;
+  }
+
+  void addTodoList(String todo) {
+    _todoList.add(todo);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'data_provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'To-do List',
       theme: ThemeData(primaryColor: Colors.white),
       home: RandomWords(),
     );
@@ -21,28 +21,75 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
   final _biggerFont = TextStyle(fontSize: 18.0);
+  final _dataProvider = DataProvider();
+  final _saved = <String>{};
 
   @override
   Widget build(BuildContext context) {
+    final data = _dataProvider.getTodoList();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator 3'),
+        title: Text('To-do List'),
         actions: [IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)],
       ),
-      body: _buildSuggestions(),
+      body: _buildList(data),
     );
+  }
+
+  Widget _buildList(List<String> data) {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemCount: data.length,
+        itemBuilder: (context, i) {
+          return _buildRow(data[i]);
+        });
+  }
+
+  Widget _buildRow(String todo) {
+    final alreadySaved = _saved.contains(todo);
+
+    final listTile = ListTile(
+      title: Text(
+        todo,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(todo);
+          } else {
+            _saved.add(todo);
+          }
+        });
+      },
+    );
+
+    return Dismissible(
+        background: Container(
+          color: Colors.red,
+        ),
+        key: UniqueKey(),
+        onDismissed: (direction) {
+          setState(() {
+            _saved.remove(todo);
+          });
+        },
+        child: listTile);
   }
 
   void _pushSaved() {
     Navigator.of(context).push(
       MaterialPageRoute<void>(builder: (BuildContext context) {
-        final tiles = _saved.map((WordPair pair) {
+        final tiles = _saved.map((String todo) {
           return ListTile(
             title: Text(
-              pair.asPascalCase,
+              todo,
               style: _biggerFont,
             ),
           );
@@ -61,56 +108,5 @@ class _RandomWordsState extends State<RandomWords> {
         );
       }),
     );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-
-    final listTile = ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-
-    return Dismissible(
-        background: Container(
-          color: Colors.red,
-        ),
-        key: UniqueKey(),
-        onDismissed: (direction) {
-          setState(() {
-            _saved.remove(pair);
-          });
-        },
-        child: listTile);
   }
 }
