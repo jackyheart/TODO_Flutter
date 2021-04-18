89f424e81026cd4628406b959a1671439ffc047e
Jacky Tjoa <jacky.coolheart@gmail.com>
2021-04-18 14:18:02 +0800

    add unit tests

---
+++ test/viewmodel_test.dart
@@ -0,0 +1,44 @@
+import 'package:test/test.dart';
+import 'package:todo_app/data_protocol.dart';
+import 'package:todo_app/data_provider.dart';
+import 'package:todo_app/todo.dart';
+
+class MockDataSource extends DataProtocol {
+  var isGetTodoListCalled = false;
+  var isAddItemCalled = false;
+  var isRemoveItemCalled = false;
+
+  Future<List<Todo>> getTodoList() {
+    isGetTodoListCalled = true;
+  }
+
+  void addItem(String todo) {
+    isAddItemCalled = true;
+  }
+
+  void removeItem(String todo) {
+    isRemoveItemCalled = true;
+  }
+}
+
+void main() {
+  MockDataSource mockDataSource = MockDataSource();
+
+  test('Test getTodoList', () {
+    final provider = DataProvider.withDataSource(mockDataSource);
+    provider.getTodoList();
+    expect(mockDataSource.isGetTodoListCalled, true);
+  });
+
+  test('Test addItem', () {
+    final provider = DataProvider.withDataSource(mockDataSource);
+    provider.addTodoList("");
+    expect(mockDataSource.isAddItemCalled, true);
+  });
+
+  test('Test removeItem', () {
+    final provider = DataProvider.withDataSource(mockDataSource);
+    provider.removeTodoItem("id");
+    expect(mockDataSource.isRemoveItemCalled, true);
+  });
+}
